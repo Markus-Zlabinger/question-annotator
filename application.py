b70e7ac205b3d616d33dce17ca687fd5006bc740
SirBlauburger <stzlab@gmail.com>
2019-08-16 19:28:56 +0200

    first commit

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import defaultsettings
 from flask import Flask, redirect, url_for, request, render_template
 from src.data_io import load_questions, load_annotations
+import csv
 import config
 import random
 #
@@ -11,7 +12,9 @@ app = Flask(__name__)
 
 # Load Global Variables
 QUESTION_POOL = None
+QUESTION_POOL_CSV=None
 SIM_MATRIX = None
+CSVpath = "annot.csv"
 
 
 """ Methods """
@@ -52,12 +55,15 @@ def remove_questions_from_pool(toremove):
 @app.before_first_request
 def initialize():
     print("INITIALIZE")
-    global ALREADY_LABELED, QUESTION_POOL, SIM_MATRIX
+    global ALREADY_LABELED, QUESTION_POOL, QUESTION_POOL_CSV, SIM_MATRIX, CSVpath
     ALREADY_LABELED = set()
     QUESTION_POOL = load_questions(config.PATH_QUESTIONS)  # [(QID1, QuestionText1), ..., (QIDN, QuestionTextN)]
+    QUESTION_POOL_CSV = load_questions(config.PATH_QUESTIONS)
     SIM_MATRIX = pickle.load(open(config.PATH_SIM_MATRIX, "rb"))
     assert len(QUESTION_POOL) == SIM_MATRIX.shape[0] == SIM_MATRIX.shape[1]
-
+    #todo schauen was bereits in file anotiert ist
+    s = csv.writer(open(CSVpath, "w", newline='', encoding="utf-8"), delimiter=",")
+    s.writerow(["Label","Qestion_ID","Query"])
 
 @app.route('/')
 def index():
@@ -67,22 +73,43 @@ def index():
 
 @app.route('/saveannotation',methods = ['POST', 'GET'])
 def saveannotation():
+    global QUESTION_POOL_CSV
+    global CSVpath
     try:
         label = request.form.get('label')
-        annotated_questions = request.form.getlist('questionlist')
+        question_ids = request.form.getlist('questionlist')
         print("##############################")
-        print("{:10s}".format("Questions"), annotated_questions)
+        print("{:10s}".format("Questions"), question_ids)
         print("{:10s}".format("Label"), label)
         print("##############################")
 
+        # for qid2, texx in QUESTION_POOL_CSV.items():
+        #     print(qid2, texx)
+        # print("##############################")
+        # Start writing to CSV
+        s = csv.writer(open(CSVpath, "a", newline='', encoding="utf-8"), delimiter=",")
+        for qid_html in question_ids:
+            print(qid_html)
+            for qid, question in QUESTION_POOL_CSV.items():
+                # print(qid, qid_html, question)
+                if qid_html == qid:
+                    print("bin in if")
+                    tex = question.strip()
+                    s.writerow([label, qid, tex])
+        # End writing to CSV
 
         # Remove questions that were already annotated from our pool
-        remove_questions_from_pool(set(annotated_questions))
+        remove_questions_from_pool(set(question_ids))
 
     except Exception as e:
         print("Something went wrong!")
         print(e)
     return index()
+
+@app.route('/test')
+def test():
+    print("hallo")
+    return "<h1>hallo</h1>"
 
 @app.route('/reset', methods = ['POST', 'GET'])
 def reset():
--- test.py
+++ test.py
@@ -1,6 +1,24 @@
-x = 3 + 2
+import config
+import csv
 
-print("Servas")
+savepath="annot.csv"
+questions = dict()
+qidx = 0
+with open(config.PATH_QUESTIONS, mode="r", encoding="utf-8") as f:
+    for index, line in enumerate(f):
+        question_text = line.strip()
+        if question_text:  # Ignore empty lines
+            questions[str(qidx)] = question_text
+            qidx += 1
 
-print("X=", x)
+# for qid, quest in questions.items():
+#    print(qid,quest)
+# for qid in questions:
+#     print(qid,questions[qid])
 
+# with open(savepath, mode="a", encoding="utf-8") as s:
+s = csv.writer(open(savepath, "w", newline='', encoding="utf-8"), delimiter=",")
+s.writerow(["Hans","Franz"])
+for qid, ling in questions.items():
+    tex = ling.strip()
+    s.writerow([qid,tex])
