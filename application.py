fa65ea52ccf1b80254f0c58b53c1b8b9a88f4ec9
SirBlauburger <stzlab@gmail.com>
2019-08-22 11:47:39 +0200

    Annotion kann nun später fortgesetzt werden

--- application.py
+++ application.py
@@ -61,9 +61,22 @@ def initialize():
     QUESTION_POOL_CSV = load_questions(config.PATH_QUESTIONS)
     SIM_MATRIX = pickle.load(open(config.PATH_SIM_MATRIX, "rb"))
     assert len(QUESTION_POOL) == SIM_MATRIX.shape[0] == SIM_MATRIX.shape[1]
-    #todo schauen was bereits in file anotiert ist
-    s = csv.writer(open(CSVpath, "w", newline='', encoding="utf-8"), delimiter=",")
-    s.writerow(["Label","Qestion_ID","Query"])
+
+    #schauen was bereits in file anotiert ist und aus Pool löschen
+    annotated = list()
+    with open(CSVpath, mode="r", encoding="utf-8") as f:
+        s = csv.reader(f, delimiter=",")
+        # print(list(s)[0][2])
+        # for label, idn, querry in s:
+        for idn in s:
+            # print(idn[1], idn[2])
+            for num, line in QUESTION_POOL.items():
+                if num == idn[1] and line == idn[2]:
+                    # print("schon enthalten: %s", num, line)
+                    annotated.append(num)
+    print("already annotated in file:", annotated)
+    # Remove questions that were already annotated in file
+    remove_questions_from_pool(set(annotated))
 
 @app.route('/')
 def index():
@@ -83,17 +96,15 @@ def saveannotation():
         print("{:10s}".format("Label"), label)
         print("##############################")
 
-        # for qid2, texx in QUESTION_POOL_CSV.items():
-        #     print(qid2, texx)
         # print("##############################")
         # Start writing to CSV
         s = csv.writer(open(CSVpath, "a", newline='', encoding="utf-8"), delimiter=",")
         for qid_html in question_ids:
-            print(qid_html)
+            # print(qid_html)
             for qid, question in QUESTION_POOL_CSV.items():
                 # print(qid, qid_html, question)
                 if qid_html == qid:
-                    print("bin in if")
+                    # print("bin in if")
                     tex = question.strip()
                     s.writerow([label, qid, tex])
         # End writing to CSV
@@ -113,6 +124,11 @@ def test():
 
 @app.route('/reset', methods = ['POST', 'GET'])
 def reset():
+    global CSVpath
+    # reset the Anotation File
+    print("reset the annotation File")
+    s = csv.writer(open(CSVpath, "w", newline='', encoding="utf-8"), delimiter=",")
+    s.writerow(["Label", "Qestion_ID", "Query"])
     initialize()
     return index()
 
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import config
 import csv
 
 savepath="annot.csv"
+# ************* aus Text File daten auslesen: ****************
 questions = dict()
 qidx = 0
 with open(config.PATH_QUESTIONS, mode="r", encoding="utf-8") as f:
@@ -22,3 +23,35 @@ s.writerow(["Hans","Franz"])
 for qid, ling in questions.items():
     tex = ling.strip()
     s.writerow([qid,tex])
+
+#*********** Einzelne daten auslesen ********************
+# zuerst oben qustions laden ausführen!!!!!!!
+vorhanden=list()
+with open(savepath, mode="r", encoding="utf-8") as f:
+    s=csv.reader(f, delimiter=",")
+    # print(list(s)[0][2])
+    # for label, idn, querry in s:
+    for idn in s:
+        # print(idn[1], idn[2])
+        for num, line in questions.items():
+            if num == idn[1] and line == idn[2]:
+                print("schon enthalten: %s", num, line)
+                vorhanden.append(num)
+print("bereits annotiert: ", vorhanden)
+#remove_questions_from_pool(set(vorhanden))
+
+#funkt:
+# vorhanden=dict()
+# qidx2 = 0
+# with open(savepath, mode="r", encoding="utf-8") as f:
+#     s=csv.reader(f, delimiter=",")
+#     # print(list(s)[0][2])
+#     for label, idn, querry in s:
+#         # print(label,querry)
+#         for num, line in questions.items():
+#             if num==idn:
+#                 # print("schon enthalten: %s", line)
+#                 vorhanden[str(qidx2)]=([label, idn, line])
+#                 qidx2+=1
+# print(vorhanden)
+
