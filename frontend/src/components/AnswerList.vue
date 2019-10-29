cc2e4e6880a0ca06b50f468d2abc1a178677af1f
Zlabinger M <markus.zlabinger@tuwien.ac.at>
2019-10-29 19:21:31 +0100

    update

---
+++ frontend/src/components/AnswerList.vue
@@ -0,0 +1,98 @@
+<template>
+    <b-card align="center">
+        <template v-slot:header>
+            <b>Select answer(s)</b>
+        </template>
+        <b-list-group>
+            <b-list-group-item>
+                <label for="search_answers">
+                    <b>Search: </b>
+                </label>
+                <input id="search_answers" type="text" v-model="search_answers"/>
+            </b-list-group-item>
+            <b-list-group-item
+                    v-for="(answer, index) in filtered_answers"
+                    :key="index"
+                    @click="answer_clicked(index)"
+                    :class="{'active': active_answers[index]}"
+                    button
+            >
+                <div class="answer">
+                    <b>{{answer.aid}}: </b>
+                    <span v-html="answer.answer"></span>
+                </div>
+            </b-list-group-item>
+        </b-list-group>
+    </b-card>
+</template>
+
+
+<script>
+    import axios from "axios";
+
+    export default {
+        name: 'AnswerList',
+        data() {
+            return {
+                search_answers: "",
+                answers: [],
+                active_answers: [],
+            };
+        },
+        mounted() {
+            const url = "http://127.0.0.1:5000/getanswers";
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response.data);
+                    this.answers = response.data.answers;
+                    this.reset_active_answers();
+                })
+                .catch(e => {
+                    console.log(e);
+                })
+        },
+        methods: {
+            reset_active_answers() {
+                this.active_answers = new Array(this.answers.length).fill(false);
+            },
+            answer_clicked(index) {
+                if (this.$store.state.multipleanswers === false) {
+                    this.reset_active_answers()
+                }
+                this.$set(this.active_answers, index, !this.active_answers[index]);
+            },
+            get_active_answers() {
+                let active_answers = [];
+                this.answers.forEach((answer, index) => {
+                    if (this.active_answers[index]) {
+                        active_answers.push(answer.aid)
+                    }
+                });
+                if (active_answers.length > 0) {
+                    this.search_answers = "";
+                }
+                return active_answers
+            },
+        },
+        computed: {
+            filtered_answers() {
+                return this.answers.filter(answer => {
+                    return (
+                        answer.answer
+                            .toLowerCase()
+                            .includes(this.search_answers.toLowerCase()) ||
+                        answer.aid
+                            .toLowerCase()
+                            .includes(this.search_answers.toLowerCase())
+                    );
+                });
+            }
+        }
+    }
+
+
+</script>
+
+<style scoped>
+</style>
